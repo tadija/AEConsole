219ff577a5fe86d9080dab1e8f356b6a8ba79dd3
Marko Tadic <tadija@me.com>
2016-03-16 22:22:04 +0100

    Work in progress

--- AELog/AELog.swift
+++ AELog/AELog.swift
@@ -1,6 +1,6 @@
 //
 //  AELog.swift
-//  TweetPlay
+//  AELog
 //
 //  Created by Marko Tadic on 3/16/16.
 //  Copyright © 2016 AE. All rights reserved.
@@ -9,16 +9,40 @@
 import Foundation
 
 func aelog(message: String = "", filePath: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-    var threadName = ""
-    threadName = NSThread.currentThread().isMainThread ? "MAIN THREAD" : (NSThread.currentThread().name ?? "UNKNOWN THREAD")
-    threadName = "[" + threadName + "] "
+    AELog.sharedInstance.log(message, filePath: filePath, line: line, function: function)
+}
+
+public protocol AELogDelegate: class {
+    func didLog(message: String)
+}
+
+public class AELog {
     
-    let fileName = NSURL(fileURLWithPath: filePath).URLByDeletingPathExtension?.lastPathComponent ?? "???"
+    // MARK: - Singleton
     
-    var msg = ""
-    if message != "" {
-        msg = " - \(message)"
+    static let sharedInstance = AELog()
+    
+    // MARK: - Properties
+    
+    weak var delegate: AELogDelegate?
+    
+    // MARK: - Actions
+    
+    func log(message: String = "", filePath: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
+        var threadName = ""
+        threadName = NSThread.currentThread().isMainThread ? "MAIN THREAD" : (NSThread.currentThread().name ?? "UNKNOWN THREAD")
+        threadName = "[" + threadName + "] "
+        
+        let fileName = NSURL(fileURLWithPath: filePath).URLByDeletingPathExtension?.lastPathComponent ?? "???"
+        
+        var msg = ""
+        if message != "" {
+            msg = " - \(message)"
+        }
+        
+        NSLog("-- " + threadName + fileName + "(\(line))" + " -> " + function + msg)
+        
+        delegate?.didLog(message)
     }
     
-    NSLog("-- " + threadName + fileName + "(\(line))" + " -> " + function + msg)
 }
