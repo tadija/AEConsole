bc4cb4a64c7e513371225bbbc9279e9ff34a9477
Marko Tadic <tadija@me.com>
2016-04-02 01:03:33 +0200

    Fixed dependencies when building on device

--- Sources/AEConsole.h
+++ Sources/AEConsole.h
@@ -23,7 +23,7 @@
 //
 
 #import <UIKit/UIKit.h>
-@import AELog;
+#import <AELog/AELog.h>
 
 FOUNDATION_EXPORT double AEConsoleVersionNumber;
 FOUNDATION_EXPORT const unsigned char AEConsoleVersionString[];
