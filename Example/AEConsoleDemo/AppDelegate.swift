6d3e8fa0fef07b48b125dc63d6b5848feef911db
Marko Tadic <tadija@me.com>
2016-10-15 17:54:16 +0200

    Updated demo project for Swift 3 and Xcode 8.1 Beta (8T47)

--- Example/AEConsoleDemo/AppDelegate.swift
+++ Example/AEConsoleDemo/AppDelegate.swift
@@ -14,29 +14,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         AEConsole.launchWithAppDelegate(self)
         aelog()
         return true
     }
     
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         aelog()
     }
     
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         aelog()
     }
     
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         aelog()
     }
     
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         aelog()
     }
     
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         aelog()
     }
 
--- Example/AEConsoleDemo/ViewController.swift
+++ Example/AEConsoleDemo/ViewController.swift
@@ -15,41 +15,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         aelog()
         generateLogLines(count: 100)
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         aelog()
         generateLogLines(count: 200)
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
         aelog()
         generateLogLines(count: 300)
     }
     
     // MARK: - Actions
 
-    @IBAction func didTapLogButton(sender: UIButton) {
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)
-        dispatch_async(queue) {
+    @IBAction func didTapLogButton(_ sender: UIButton) {
+        let queue = DispatchQueue.global()
+        queue.async {
             self.generateLogLines(count: Int.random(max: 1000))
-            dispatch_async(dispatch_get_main_queue(), {
+            DispatchQueue.main.async(execute: {
                 aelog(sender)
             })
         }
     }
     
-    @IBAction func didTapToggleButton(sender: UIButton) {
+    @IBAction func didTapToggleButton(_ sender: UIButton) {
         AEConsole.toggle()
     }
     
     // MARK: - Helpers
     
-    func generateLogLines(count count: Int) {
+    func generateLogLines(count: Int) {
         for i in 0...count {
             aelog("I'm just a log line #\(i).")
         }
@@ -58,7 +61,7 @@ class ViewController: UIViewController {
 }
 
 extension Int {
-    static func random(min: Int = 0, max: Int = Int.max) -> Int {
+    static func random(_ min: Int = 0, max: Int = Int.max) -> Int {
         return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
     }
 }
