a82ccbc2bd67b6e5949d8da95fd5496080295c96
Marko Tadic <tadija@me.com>
2016-10-16 15:38:01 +0200

    Fixed demo project

--- Example/AEConsoleDemo/AppDelegate.swift
+++ Example/AEConsoleDemo/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        AEConsole.launchWithAppDelegate(self)
+        AEConsole.launch(with: self)
         aelog()
         return true
     }
